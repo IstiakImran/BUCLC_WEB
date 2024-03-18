b6c30345add8cc1833832e190ca8b63227a50e4a
IstiakImran <toufique.hassan.71@gmail.com>
2024-03-18 23:38:35 +0600

    event data revised

--- src/Pages/Campaign/SpeakBold/SpeakBold.jsx
+++ src/Pages/Campaign/SpeakBold/SpeakBold.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import heroBaner from "../../../assets/SpeakBold/HeroBanner.png";
 import aboutImg from "../../../assets/SpeakBold/SpeakBoldAbout.png";
+import speakBold from "../../../assets/SpeakBold/SpeakBold.png";
 import "./SpeakBold.css";
 
 const SpeakBold = () => {
@@ -8,7 +9,8 @@ const SpeakBold = () => {
     <div className="speak-bold-container">
       <header className="speak-bold">
         <div className="speak-bold-text">
-          <h1>Speak Bold</h1>
+          {/* <h1>Speak Bold</h1> */}
+          <img src={speakBold} alt="" />
           <p>Unlocking the Power of Effective Communication</p>
         </div>
         <img className="speak-bold-img" src={heroBaner} alt="" />
@@ -20,22 +22,36 @@ const SpeakBold = () => {
         <div className="speak-bold-about-text">
           <h1>About the Workshop</h1>
           <p>
-            A brief overview highlighting the workshop's aim to explore
-            effective communication keys and overcoming the fear of public
-            speaking. Mention it's suitable for both seasoned professionals and
-            newcomers.
+            "Communication – the human connection – is the key to personal and
+            career success." ~ Paul J. Meyer With this being said, here comes
+            the most intriguing question: What makes you quiet all of a sudden
+            when you desperately want to say something? Why does your heart skip
+            a bit when you almost begin to say the very first word of your
+            speech or a random presentation in front of an audience?
+            Nervousness, shyness, rapid sweating, abrupt mind blankness & so
+            many other attacks strike you both mentally and physically, isn’t
+            it? Hence, to help you fight against these ambushes, BUCLC will be
+            organizing an electrifying workshop “Speak Bold” to electrify you &
+            boost you up mentally as an effective speaker as well as a better
+            communicator with people from all walks of life. So what are you
+            waiting for? Join us for an engaging and informative event, "Speak
+            Bold," on 25th March, 2024 Together, let's explore the keys to
+            effective communication and overcoming the fear of public speaking.
+            Whether you're a seasoned professional or a newcomer to public
+            speaking, this event offers valuable insights and practical tips to
+            enhance your communication skills and boost your confidence.
+            Essential details are given down below 👇🏼
           </p>
           <div className="speak-bold-about-location">
             <p>
               <span>Location:</span> To Be Announced
             </p>
           </div>
-          
         </div>
       </section>
 
       <section className="speak-bold-schedule">
-        <h1 className="speak-bold-schedule-title" >Workshop Schedule</h1>
+        <h1 className="speak-bold-schedule-title">Workshop Schedule</h1>
         <div className="speak-bold-schedule-container">
           <div className="speak-bold-schedule-item">
             <h1>First Part</h1>
@@ -51,8 +67,11 @@ const SpeakBold = () => {
           </div>
           <div className="speak-bold-schedule-item">
             <h1>
-              Break: Refreshments and networking opportunity (1:00 PM - 1:10 PM)
+              Break
             </h1>
+            <p>
+            Refreshments and networking opportunity (1:00 PM - 1:10 PM)
+            </p>
           </div>
           <div className="speak-bold-schedule-item">
             <h1> Second Part</h1>
